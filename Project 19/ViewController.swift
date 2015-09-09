cc607770d6669bc8fb8d0504c4d563b17472434c
engjasonp <engjasonp@gmail.com>
2015-09-08 18:10:45 -0700

    Added func mapView viewForAnnotation and func mapView nnotationView.

--- Project 19/ViewController.swift
+++ Project 19/ViewController.swift
@@ -20,18 +20,47 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
         
-        mapView.addAnnotation(london)
-        mapView.addAnnotation(oslo)
-        mapView.addAnnotation(paris)
-        mapView.addAnnotation(rome)
-        mapView.addAnnotation(washington)
+//        mapView.addAnnotation(london)
+//        mapView.addAnnotation(oslo)
+//        mapView.addAnnotation(paris)
+//        mapView.addAnnotation(rome)
+//        mapView.addAnnotation(washington)
+        
+        mapView.addAnnotations([london, oslo, paris, rome, washington])
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        let identifier = "Capital"
+        
+        if annotation.isKindOfClass(Capital.self) {
+            var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+            
+            if annotationView == nil {
+                annotationView = MKPinAnnotationView(annotation:annotation, reuseIdentifier:identifier)
+                annotationView.enabled = true
+                annotationView.canShowCallout = true
+                
+                let btn = UIButton.buttonWithType(.DetailDisclosure) as! UIButton
+                annotationView.rightCalloutAccessoryView = btn
+            } else {
+                annotationView.annotation = annotation
+            }
+            
+            return annotationView
+        }
+        
+        return nil
     }
-
+    
+    func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {
+        let capital = view.annotation as! Capital
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+    }
 
 }
 
